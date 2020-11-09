9b4bd19230a7fc68467f1b7ac0881827952a15d2
dimitriossakkas <dimisak7@gmail.com>
2020-11-09 11:30:13 +0100

    Initial commit

---
+++ src/test/java/com/dimi/plangenerator/PlangeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimi.plangenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlangeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
